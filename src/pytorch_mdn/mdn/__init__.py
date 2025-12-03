979b970a31d339a8225dce7a309d2cbbc3690326
Nima Nooshiri <nima.nooshiri@gmail.com>
2025-12-03 17:08:26 +0100

    chore: add polars to dependencies

--- src/pytorch_mdn/mdn/__init__.py
+++
@@ -1,6 +0,0 @@
-from .config import MDNTrainingConfig
-from .loss import mdn_loss
-from .model import MixtureDensityNetwork
-from .trainer import MDNLitModule
-
-__all__ = ["MDNLitModule", "MDNTrainingConfig", "MixtureDensityNetwork", "mdn_loss"]
